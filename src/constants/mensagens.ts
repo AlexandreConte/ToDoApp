1d1ba7e28b6075849847ed33eb67ebf1671a97cc
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-05-19 20:48:27 -0300

    implementa controlador

---
+++ src/constants/mensagens.ts
@@ -0,0 +1,2 @@
+export const esperaUsuario = "Aperte 'enter' para continuar..."
+export const nenhumaLista = "Nenhuma lista de tarefas foi cadastrada!"
---
+++ src/controllers/Controlador.ts
@@ -0,0 +1,17 @@
+export default class Controlador {
+  constructor(
+    private _manterAberto: boolean = true,
+  ) { }
+
+  get manterAberto(): boolean {
+    return this._manterAberto
+  }
+
+  abrir() {
+    this._manterAberto = true
+  }
+
+  fechar() {
+    this._manterAberto = false
+  }
+}
