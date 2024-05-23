d75fda4bc054c65fb68fa451151c05f940b83059
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-05-23 13:58:15 -0300

    remove pasta models

--- src/models/Entidade.ts
+++
@@ -1,9 +0,0 @@
-import Id from "./Id"
-
-export default class Entidade {
-  constructor(private readonly _id = Id.novo()) { }
-
-  get Id() {
-    return this._id
-  }
-}
--- src/models/Id.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid"
-
-export default class Id {
-  private static _id: string
-
-  static novo() {
-    return uuidv4()
-  }
-}
